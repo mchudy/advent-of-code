a733ebf7789345977d2067a6440a322020b67709
Marcin Chudy <marcinchudy94@gmail.com>
2020-12-17 20:11:32 +0100

    Add day 17 part 2

--- 2020/17/17.go
+++ 2020/17/17.go
@@ -14,38 +14,46 @@ const (
 
 // Grid -
 type Grid struct {
-	fields          []bool
-	w, h, d, offset int
+	fields             []bool
+	w, h, d, t, offset int
 }
 
-func createGrid(w, h, d, offset int) *Grid {
+func createGrid(w, h, d, t, offset int) *Grid {
 	return &Grid{
-		fields: make([]bool, w*h*d),
+		fields: make([]bool, w*h*d*t),
 		w:      w,
 		h:      h,
 		d:      d,
+		t:      t,
 		offset: offset,
 	}
 }
 
-func (t *Grid) at(x, y, z int) bool {
-	if x+t.offset < 0 || x+t.offset > t.w-1 || y+t.offset < 0 || y+t.offset > t.h-1 || z+t.offset < 0 || z+t.offset > t.d-1 {
+func (g *Grid) isIndexValid(x, y, z, t int) bool {
+	if x+g.offset < 0 || x+g.offset > g.w-1 || y+g.offset < 0 || y+g.offset > g.h-1 || z+g.offset < 0 || z+g.offset > g.d-1 || t+g.offset < 0 || t+g.offset > g.t-1 {
 		return false
 	}
-
-	idx := t.h*t.w*(z+t.offset) + t.w*(y+t.offset) + x + t.offset
-	if idx < 0 || idx > t.w*t.h*t.d-1 {
-		return false
-	}
-	return t.fields[idx]
+	return true
 }
 
-func (t *Grid) set(x, y, z int, val bool) {
-	if x+t.offset < 0 || x+t.offset > t.w-1 || y+t.offset < 0 || y+t.offset > t.h-1 || z+t.offset < 0 || z+t.offset > t.d-1 {
-		println("Something wrong")
+func (g *Grid) at(x, y, z, t int) bool {
+	if !g.isIndexValid(x, y, z, t) {
+		return false
 	}
-	idx := t.h*t.w*(z+t.offset) + t.w*(y+t.offset) + x + t.offset
-	t.fields[idx] = val
+
+	idx := g.h*g.w*g.d*(t+g.offset) + g.h*g.w*(z+g.offset) + g.w*(y+g.offset) + x + g.offset
+	if idx < 0 || idx > g.t*g.w*g.h*g.d-1 {
+		return false
+	}
+	return g.fields[idx]
+}
+
+func (g *Grid) set(x, y, z, t int, val bool) {
+	if !g.isIndexValid(x, y, z, t) {
+		print("Something wrong")
+	}
+	idx := g.h*g.w*g.d*(t+g.offset) + g.h*g.w*(z+g.offset) + g.w*(y+g.offset) + x + g.offset
+	g.fields[idx] = val
 }
 
 func readLines(path string) []string {
@@ -65,46 +73,49 @@ func readLines(path string) []string {
 }
 
 func parseInput(lines []string) *Grid {
-	grid := createGrid(len(lines[0]), len(lines), 1, 0)
+	grid := createGrid(len(lines[0]), len(lines), 1, 1, 0)
 	for y, line := range lines {
 		for x, field := range line {
-			grid.set(x, y, 0, field == Active)
+			grid.set(x, y, 0, 0, field == Active)
 		}
 	}
 	return grid
 }
 
-func printGrid(grid *Grid) {
-	for z := 0; z < grid.d; z++ {
-		println("z =", z)
-		for y := 0; y < grid.h; y++ {
-			for x := 0; x < grid.w; x++ {
-				if grid.at(x-grid.offset, y-grid.offset, z-grid.offset) {
-					print(string(Active))
-				} else {
-					print(string(Inactive))
-				}
-			}
-			println()
-		}
-		println()
-	}
-}
+// func printGrid(grid *Grid) {
+// 	for t := 0; t < grid.t; t++ {
+// 		for z := 0; z < grid.d; z++ {
+// 			for y := 0; y < grid.h; y++ {
+// 				for x := 0; x < grid.w; x++ {
+// 					if grid.at(x-grid.offset, y-grid.offset, z-grid.offset, t-grid.offset) {
+// 						print(string(Active))
+// 					} else {
+// 						print(string(Inactive))
+// 					}
+// 				}
+// 				println()
+// 			}
+// 			println()
+// 		}
+// 	}
+// }
 
-func getNewCubeState(grid *Grid, x, y, z int) bool {
+func getNewCubeState(grid *Grid, x, y, z, t int) bool {
 	activeNeighbors := 0
 	for i := x - 1; i <= x+1; i++ {
 		for j := y - 1; j <= y+1; j++ {
 			for k := z - 1; k <= z+1; k++ {
-				if x != i || y != j || z != k {
-					if grid.at(i, j, k) {
-						activeNeighbors++
+				for l := t - 1; l <= t+1; l++ {
+					if x != i || y != j || z != k || l != t {
+						if grid.at(i, j, k, l) {
+							activeNeighbors++
+						}
 					}
 				}
 			}
 		}
 	}
-	currentlyActive := grid.at(x, y, z)
+	currentlyActive := grid.at(x, y, z, t)
 	if currentlyActive && (activeNeighbors == 2 || activeNeighbors == 3) {
 		return true
 	}
@@ -115,16 +126,18 @@ func getNewCubeState(grid *Grid, x, y, z int) bool {
 }
 
 func expandGrid(grid *Grid, offset int) (*Grid, int) {
-	newGrid := createGrid(grid.w+2, grid.h+2, grid.d+2, offset)
+	newGrid := createGrid(grid.w+2, grid.h+2, grid.d+2, grid.t+2, offset)
 
 	activeSum := 0
-	for z := 0; z < newGrid.d; z++ {
-		for y := 0; y < newGrid.h; y++ {
-			for x := 0; x < newGrid.w; x++ {
-				active := getNewCubeState(grid, x-offset, y-offset, z-offset)
-				newGrid.set(x-newGrid.offset, y-newGrid.offset, z-newGrid.offset, active)
-				if active {
-					activeSum++
+	for t := 0; t < newGrid.t; t++ {
+		for z := 0; z < newGrid.d; z++ {
+			for y := 0; y < newGrid.h; y++ {
+				for x := 0; x < newGrid.w; x++ {
+					active := getNewCubeState(grid, x-offset, y-offset, z-offset, t-offset)
+					newGrid.set(x-newGrid.offset, y-newGrid.offset, z-newGrid.offset, t-newGrid.offset, active)
+					if active {
+						activeSum++
+					}
 				}
 			}
 		}
@@ -135,11 +148,11 @@ func expandGrid(grid *Grid, offset int) (*Grid, int) {
 
 func part1(lines []string) int {
 	grid := parseInput(lines)
-	printGrid(grid)
+	// printGrid(grid)
 	activeSum := 0
 	for i := 0; i < 6; i++ {
 		grid, activeSum = expandGrid(grid, (i + 1))
-		printGrid(grid)
+		// printGrid(grid)
 	}
 	return activeSum
 }
